3ec0348fa82c4c3ecd37b89ef06bb7ce1ca1e5ff
Camila Rezende <camila.salinorezende@everis.nttdata.com>
2022-02-01 11:18:18 -0300

    Adicionar arquivos de projeto.

---
+++ src/SGFornecedor.applicationCore/Entity/Fornecedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGFornecedor.applicationCore.Entity
+{
+    internal class Fornecedor
+    {
+        public Guid Id { get; set; }
+        public bool Active { get; set; }
+        public DateTime InsertDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+
+    }
+}
